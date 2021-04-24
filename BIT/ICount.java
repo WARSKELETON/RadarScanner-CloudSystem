417755b946e26319fe57a98bf7de80eb684cadda
WARSKELETON <yugi.man22@gmail.com>
2021-04-24 13:09:05 +0100

    Collecting instruction count per thread

--- BIT/ICount.java
+++ BIT/ICount.java
@@ -1,9 +1,9 @@
-	/* ICount.java
+/* ICount.java
  * Sample program using BIT -- counts the number of instructions executed.
  *
  * Copyright (c) 1997, The Regents of the University of Colorado. All
  * Rights Reserved.
- * 
+ *
  * Permission to use and copy this software and its documentation for
  * NON-COMMERCIAL purposes and without fee is hereby granted provided
  * that this copyright notice appears in all copies. If you wish to use
@@ -21,42 +21,44 @@ import java.util.*;
 public class ICount {
     private static PrintStream out = null;
     private static int i_count = 0;
-    
+
     /* main reads in all the files class files present in the input directory,
      * instruments them, and outputs them to the specified output directory.
      */
     public static void main(String argv[]) {
         File file_in = new File(argv[0]);
         String infilenames[] = file_in.list();
-        
+
         for (int i = 0; i < infilenames.length; i++) {
             String infilename = infilenames[i];
             if (infilename.endsWith("Solver.class") || infilename.equals("SolverArgumentParser.class") || infilename.equals("Solver$Viewport.class") || infilename.equals("SolverArgumentParser$SolverParameters.class") || infilename.equals("SolverMain.class")) {
-				// create class info object
-				ClassInfo ci = new ClassInfo(argv[0] + System.getProperty("file.separator") + infilename);
-				
+                // create class info object
+                ClassInfo ci = new ClassInfo(argv[0] + System.getProperty("file.separator") + infilename);
+
                 // loop through all the routines
                 // see java.util.Enumeration for more information on Enumeration class
                 for (Enumeration e = ci.getRoutines().elements(); e.hasMoreElements(); ) {
                     Routine routine = (Routine) e.nextElement();
-					routine.addBefore("ICount", "count", new Integer(routine.getInstructionCount()));
+                    routine.addBefore("pt/ulisboa/tecnico/cnv/server/WebServer", "count", new Integer(routine.getInstructionCount()));
 
                     if (routine.getMethodName().equals("solveImage")) {
-                        routine.addAfter("ICount", "printICount", ci.getClassName());
+                        routine.addAfter("pt/ulisboa/tecnico/cnv/server/WebServer", "testPrint", 0);
                     }
                 }
                 ci.write(argv[1] + System.getProperty("file.separator") + infilename);
             }
         }
     }
-    
+
     public static synchronized void printICount(String foo) {
         System.out.println(i_count + " instructions");
     }
-    
 
     public static synchronized void count(int incr) {
         i_count += incr;
     }
-}
 
+    public static synchronized int getCount() {
+        return i_count;
+    }
+}
--- BIT/StatisticsBranch.java
+++ BIT/StatisticsBranch.java
@@ -8,45 +8,44 @@
 // ALL RIGHTS RESERVED.
 //
 // Permission to use, copy, modify, and distribute this software and its
-// documentation for non-commercial purposes is hereby granted provided 
+// documentation for non-commercial purposes is hereby granted provided
 // that this copyright notice appears in all copies.
-// 
+//
 // This software is provided "as is".  The licensor makes no warrenties, either
 // expressed or implied, about its correctness or performance.  The licensor
 // shall not be liable for any damages suffered as a result of using
 // and modifying this software.
 package BIT;
 
-public class StatisticsBranch 
+public class StatisticsBranch
 {
-	String class_name_;
-	String method_name_;
-	int pc_;
-	int taken_;
-	int not_taken_;
+    String class_name_;
+    String method_name_;
+    int pc_;
+    int taken_;
+    int not_taken_;
 
-	public StatisticsBranch(String class_name, String method_name, int pc) 
-		{
-			class_name_ = class_name;
-			method_name_ = method_name;
-			pc_ = pc;
-			taken_ = 0;
-			not_taken_ = 0;
-		}
+    public StatisticsBranch(String class_name, String method_name, int pc)
+    {
+        class_name_ = class_name;
+        method_name_ = method_name;
+        pc_ = pc;
+        taken_ = 0;
+        not_taken_ = 0;
+    }
 
-	public void print() 
-		{
-			System.out.println(class_name_ + '\t' + method_name_ + '\t' + pc_ + '\t' + taken_ + '\t' + not_taken_);
-		}
-	
-	public void incrTaken()
-		{
-			taken_++;
-		}
+    public void print()
+    {
+        System.out.println(class_name_ + '\t' + method_name_ + '\t' + pc_ + '\t' + taken_ + '\t' + not_taken_);
+    }
 
-	public void incrNotTaken() 
-		{
-			not_taken_++;
-		}
+    public void incrTaken()
+    {
+        taken_++;
+    }
+
+    public void incrNotTaken()
+    {
+        not_taken_++;
+    }
 }
-
