c4608b12f1679d1cea8de47331878613d7c02d55
WARSKELETON <yugi.man22@gmail.com>
2021-05-15 23:56:44 +0100

    Added DynamoDB service

---
+++ pt/ulisboa/tecnico/cnv/dynamodb/DynamoDBService.java
@@ -0,0 +1,101 @@
+package pt.ulisboa.tecnico.cnv.dynamodb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeTableRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.dynamodbv2.model.TableDescription;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+
+public class DynamoDBService {
+
+    static AmazonDynamoDB dynamoDB;
+    public static String tableName = "scan-requests-table";
+    private DynamoDBMapper mapper;
+
+    private static void init() throws Exception {
+        ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                            "Please make sure that your credentials file is at the correct " +
+                            "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        dynamoDB = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion("us-east-1")
+                .build();
+
+        mapper = new DynamoDBMapper(dynamoDB);
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        try {
+            CreateTableRequest createTableRequest = new CreateTableRequest().withTableName(tableName)
+                    .withKeySchema(new KeySchemaElement().withAttributeName("id").withKeyType(KeyType.HASH))
+                    .withAttributeDefinitions(new AttributeDefinition().withAttributeName("id").withAttributeType(ScalarAttributeType.S))
+                    .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(1L).withWriteCapacityUnits(1L));
+
+            TableUtils.createTableIfNotExists(dynamoDB, createTableRequest);
+            TableUtils.waitUntilActive(dynamoDB, tableName);
+
+            // Describe our new table
+            DescribeTableRequest describeTableRequest = new DescribeTableRequest().withTableName(tableName);
+            TableDescription tableDescription = dynamoDB.describeTable(describeTableRequest).getTable();
+            System.out.println("Table Description: " + tableDescription);
+
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to AWS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with AWS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+
+    public static List<Request> getRequestById(Request request) {
+        DynamoDBQueryExpression<Request> queryExpression = new DynamoDBQueryExpression<Request>()
+                .withHashKeyValues(request);
+        return mapper.query(Request.class, queryExpression);
+    }
+
+    public static void saveRequest(Request request) {
+        mapper.save(request);
+    }
+
+}
--- pt/ulisboa/tecnico/cnv/server/Request.java
+++ pt/ulisboa/tecnico/cnv/server/Request.java
@@ -1,8 +1,13 @@
 package pt.ulisboa.tecnico.cnv.server;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
+@DynamoDBTable(tableName = "scan-requests-table")
 public class Request {
 
+    private String id;
     private int width;
     private int height;
     private int viewportTopLeftX;
@@ -52,21 +57,101 @@ public class Request {
                     //should not reach here
             }
         }
+        this.id = buildRequestId(args);
         this.metrics = new Metrics();
     }
 
+    public void buildRequestId(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (String string : args) {
+            builder.append(string);
+        }
+        this.setId(builder.toString());
+        System.out.println("Request ID: " + id);
+    }
+
     //Getters
+
+    @DynamoDBHashKey(attributeName = "id")
+    public String getId() {
+        return id;
+    }
+
+    @DynamoDBAttribute(attributeName = "width")
     public int getWidth() { return width; }
+
+    @DynamoDBAttribute(attributeName = "height")
     public int getHeight() { return height; }
+
+    @DynamoDBAttribute(attributeName = "x0")
     public int getViewportTopLeftX() { return viewportTopLeftX; }
+
+    @DynamoDBAttribute(attributeName = "y0")
     public int getViewportTopLeftY() { return viewportTopLeftY; }
+
+    @DynamoDBAttribute(attributeName = "x1")
     public int getViewportBottomRightX() { return viewportBottomRightX; }
+
+    @DynamoDBAttribute(attributeName = "y1")
     public int getViewportBottomRightY() { return viewportBottomRightY; }
+
+    @DynamoDBAttribute(attributeName = "xS")
     public int getStartingPointX() { return startingPointX; }
+
+    @DynamoDBAttribute(attributeName = "xY")
     public int getStartingPointY() { return startingPointY; }
+
+    @DynamoDBAttribute(attributeName = "strategy")
     public String getStrategy() { return strategy; }
+
+    @DynamoDBAttribute(attributeName = "image")
     public String getImage() { return image; }
+
     public Metrics getMetrics() { return metrics; }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setViewportTopLeftX(int viewportTopLeftX) {
+        this.viewportTopLeftX = viewportTopLeftX;
+    }
+
+    public void setViewportTopLeftY(int viewportTopLeftY) {
+        this.viewportTopLeftY = viewportTopLeftY;
+    }
+
+    public void setViewportBottomRightX(int viewportBottomRightX) {
+        this.viewportBottomRightX = viewportBottomRightX;
+    }
+
+    public void setViewportBottomRightY(int viewportBottomRightY) {
+        this.viewportBottomRightY = viewportBottomRightY;
+    }
+
+    public void setStartingPointX(int startingPointX) {
+        this.startingPointX = startingPointX;
+    }
+
+    public void setStartingPointY(int startingPointY) {
+        this.startingPointY = startingPointY;
+    }
+
+    public void setStrategy(String strategy) {
+        this.strategy = strategy;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 
     @Override
     public String toString() {
