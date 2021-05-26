d2dabbf6b89c97f4359a308e32de2634197dcbef
WARSKELETON <yugi.man22@gmail.com>
2021-05-26 15:06:56 +0100

    Fixed output

--- pt/ulisboa/tecnico/cnv/controller/AutoScaler.java
+++ pt/ulisboa/tecnico/cnv/controller/AutoScaler.java
@@ -230,7 +230,7 @@ public class AutoScaler {
 
     private void printStatusToFile(int numberOfWorkers, double averageCurrentWorkload, double averageCPUUtilization) {
         try {
-            String outputStr = "numberOfWorkers: " + numberOfWorkers + "\n averageCurrentWorkload: " + averageCurrentWorkload + "\n averageCPUUtilization: " + averageCPUUtilization;
+            String outputStr = "numberOfWorkers: " + numberOfWorkers + "\n averageCurrentWorkload: " + averageCurrentWorkload + "\n averageCPUUtilization: " + averageCPUUtilization + "\n";
             FileOutputStream outputStream = new FileOutputStream("scaler.txt", true);
             byte[] strToBytes = outputStr.getBytes();
             outputStream.write(strToBytes);
